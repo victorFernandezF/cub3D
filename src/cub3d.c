a0c39caba06c9d8e668fd3a0c17e0b17fcbb6296
admin <admin@iMac-de-Administrador.local>
2023-10-26 12:35:12 +0200

    restore cub3d.c by conflicts on merge

---
+++ src/cub3d.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fortega- <fortega-@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/07 12:03:35 by fortega-          #+#    #+#             */
+/*   Updated: 2023/10/26 12:34:15 by fortega-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+void	ft_leaks(void);
+
+int	cb_exit(t_core *core)
+{
+	cb_freecore(core);
+	exit (0);
+	return (0);
+}
+
+int	cb_input(int key, t_core *core)
+{
+	if (key == 53)
+		cb_exit(core);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_core	core;
+
+	if (argc != 2 || (argc == 2 && !argv[1]))
+	{
+		printf("Error\nNeeded .cub map\n");
+		return (0);
+	}
+	atexit(ft_leaks);
+	check_max_speed();
+	core = cb_init(argv);
+	core.player = init_player_datas(core);
+	printcore(core);
+	mlx_hook(core.win, 17, 0, cb_exit, &core);
+	mlx_hook(core.win, 2, 0, input, &core);
+	core.player = rc_start(core);
+	mlx_loop(core.mlx);
+	cb_freecore(&core);
+	return (0);
+}
